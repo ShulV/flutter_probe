2dba8eeb54dbdcfab0763232791f2d4919c12184
ShulV <vshulpov@gmail.com>
2024-10-20 12:18:07 +0700

    add simple example with base elements, onPressed event + own font

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-  home: Text("KEK")
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Xmap"),
+          backgroundColor: Colors.deepOrangeAccent,
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text("Some spot info", style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+            fontFamily: "PlaywriteGBS-Regular"
+          ),),
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.deepOrangeAccent,
+            onPressed: () {
+              print("CLICKED!");
+            },
+            child: Text("Click")),
+      ),
+    );
+  }
+}

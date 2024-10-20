0df665aac7d73792b9714cfc7bc73f4be6cbea18
ShulV <vshulpov@gmail.com>
2024-10-20 15:47:48 +0700

    add images from network and assets

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -13,13 +13,30 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepOrangeAccent,
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text("Some spot info", style: TextStyle(
-            fontSize: 20,
-            color: Colors.black,
-            fontFamily: "PlaywriteGBS-Regular"
-          ),),
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              child: Image.network("https://www.amic.ru/images/post_gallery/gallery/3396/40072/large.jpg?_=1994635747"),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              child: Image.asset("assets/images/skatepark_asset.jpg"),
+            )
+          ],
         ),
+
+        
+        // const Center(
+        //
+          // child: Text("Some spot info", style: TextStyle(
+          //   fontSize: 20,
+          //   color: Colors.black,
+          //   fontFamily: "PlaywriteGBS-Regular"
+          // ),),
+        // ),
+        
+        
         floatingActionButton: FloatingActionButton(
             backgroundColor: Colors.deepOrangeAccent,
             onPressed: () {

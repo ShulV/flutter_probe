a7730b67c100d6c09ce90b1fadc71eece71a3b49
ShulV <vshulpov@gmail.com>
2024-10-22 23:35:49 +0700

    navigation bar example

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -1,80 +1,142 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+/// Flutter code sample for [NavigationBar].
 
-class MyApp extends StatelessWidget {
+void main() => runApp(const NavigationBarApp());
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Xmap"),
-          backgroundColor: Colors.deepOrangeAccent,
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(20.0),
-              child: Image.network("https://www.amic.ru/images/post_gallery/gallery/3396/40072/large.jpg?_=1994635747"),
-            ),
-            Container(
-              margin: const EdgeInsets.all(20.0),
-              child: Image.asset("assets/images/skatepark_asset.jpg"),
-            ),
-            // === ряд кнопок иконок ===========================================
-            Container(
-              margin: const EdgeInsets.all(20.0),
-              child:
+      theme: ThemeData(useMaterial3: true),
+      home: const NavigationExample(),
+    );
+  }
+}
 
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
 
-              Row( // Используем Row для расположения кнопок по горизонтали
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: const Icon(
-                        Icons.favorite,
-                        size: 64, color:
-                    Colors.deepOrangeAccent),
-                    onPressed: () {
-                      // Действие при нажатии
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                        Icons.star,
-                        size: 72, color:
-                    Colors.amber),
-                    onPressed: () {
-                      // Действие при нажатии
-                    },
-                  ),
-                ],
-              ),
-              // ===============================================================
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('2'),
+              child: Icon(Icons.messenger_sharp),
             ),
-          ],
-        ),
-
-        
-        // const Center(
-        //
-          // child: Text("Some spot info", style: TextStyle(
-          //   fontSize: 20,
-          //   color: Colors.black,
-          //   fontFamily: "PlaywriteGBS-Regular"
-          // ),),
-        // ),
-        
-        
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.deepOrangeAccent,
-            onPressed: () {
-              print("CLICKED!");
-            },
-            child: Text("Click")),
+            label: 'Messages',
+          ),
+        ],
       ),
+      body: <Widget>[
+        /// Home page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Home page',
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+
+        /// Notifications page
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 1'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 2'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// Messages page
+        ListView.builder(
+          reverse: true,
+          itemCount: 2,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  margin: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.primary,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Text(
+                    'Hello',
+                    style: theme.textTheme.bodyLarge!
+                        .copyWith(color: theme.colorScheme.onPrimary),
+                  ),
+                ),
+              );
+            }
+            return Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.primary,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  'Hi!',
+                  style: theme.textTheme.bodyLarge!
+                      .copyWith(color: theme.colorScheme.onPrimary),
+                ),
+              ),
+            );
+          },
+        ),
+      ][currentPageIndex],
     );
   }
 }
